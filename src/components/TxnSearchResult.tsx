ea53fe5720e1679b33e0c710100ba96211b8ff06
Toan Ho <toanbku@gmail.com>
2023-05-14 13:51:42 +0700

    fix: relative img

--- src/components/TxnSearchResult.tsx
+++ src/components/TxnSearchResult.tsx
@@ -156,7 +156,7 @@ export const TxnSearchResult = ({ data }: { data: ISearchResult }) => {
               className="block w-full p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
             >
               <h5 className="flex items-center gap-1 md:gap-[6px] mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-                <div className="w-[26px] h-[26px]">
+                <div className="relative w-[26px] h-[26px]">
                   <Image
                     src={CHAIN_DETAIL[chainName].logo || DEFAULT_CHAIN_LOGO}
                     fill

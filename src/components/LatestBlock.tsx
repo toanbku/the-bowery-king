48e3d19b8ce933ce85c0046b91880bedbaa50f4e
Toan Ho <toanbku@gmail.com>
2023-05-14 13:49:46 +0700

    chore: hidden zero value

--- src/components/LatestBlock.tsx
+++ src/components/LatestBlock.tsx
@@ -33,20 +33,22 @@ export const LatestBlock = ({
         {response ? (
           <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
             <tbody>
-              {mappingData(response)?.map((item) => (
-                <tr
-                  key={item.label}
-                  className="border-b dark:bg-gray-800 dark:border-gray-700"
-                >
-                  <th
-                    scope="row"
-                    className="py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              {mappingData(response)
+                ?.filter((item) => item.value)
+                .map((item) => (
+                  <tr
+                    key={item.label}
+                    className="border-b dark:bg-gray-800 dark:border-gray-700"
                   >
-                    {item.label}
-                  </th>
-                  <td className="px-6 py-4">{item.value}</td>
-                </tr>
-              ))}
+                    <th
+                      scope="row"
+                      className="py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    >
+                      {item.label}
+                    </th>
+                    <td className="px-6 py-4">{item.value}</td>
+                  </tr>
+                ))}
             </tbody>
           </table>
         ) : (
